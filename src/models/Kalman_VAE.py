d7a0358d9b7ac3d10a5a4c40cc4dfc35e666db3d
jysonganan <jysongane@gmail.com>
2024-10-13 22:56:02 -0700

    modify kalmanVAE

--- src/models/Kalman_VAE.py
+++ src/models/Kalman_VAE.py
@@ -16,8 +16,7 @@ class KalmanVAE(nn.Module):
                                     )
         self.decoder = nn.Sequential(nn.Linear(latent_dim, 256),
                                      nn.ReLU(),
-                                     nn.Linear(256, input_dim),
-                                     nn.sigmoid()
+                                     nn.Linear(256, input_dim)
                                     )
         
         self.transition_matrix = nn.Parameter(torch.eye(latent_dim))
@@ -32,36 +31,36 @@ class KalmanVAE(nn.Module):
     
     def forward(self, x):
         batch_size, seq_len, _ = x.size()
-        z_means, z_logvars, z_preds, z_updateds = [], [], [], []
+        mean, logvar, z_preds, z_updateds = [], [], [], []
         z_prev = torch.zeros(batch_size, self.latent_dim).to(x.device)
         for t in range(seq_len):
             x_t = x[:,t,:]
-            z_mean, z_logvar = torch.chunk(self.encoder(x_t), 2, dim=-1)
-            z_means.append(z_mean)
-            z_logvars.append(z_logvar)
-            z = self.reparameterize(z_mean, z_logvar)
+            mean_t, logvar_t = torch.chunk(self.encoder(x_t), 2, dim=-1)
+            mean.append(mean_t)
+            logvar.append(logvar_t)
+            
+            z_t = self.reparameterize(mean_t, logvar_t)
 
             # Kalman Filtering
-            z_pred = torch.matmul(self.transition_matrix, z_prev.t()).t() + z
-            z_preds.append(z_pred)
+            z_pred_t = torch.matmul(self.transition_matrix, z_prev.t()).t() + z_t
+            z_preds.append(z_pred_t)
 
             z_pred_cov = torch.matmul(self.transition_matrix, self.process_covariance)
-            innovation = x_t - torch.matmul(self.measurement_matrix, z_pred.t()).t()
+            innovation = x_t - torch.matmul(self.measurement_matrix, z_pred_t.t()).t()
             innovation_cov = torch.matmul(self.measurement_matrix, z_pred_cov) + self.measurement_covariance
             kalman_gain = torch.matmul(z_pred_cov, torch.inverse(innovation_cov))
-            z_updated = z_pred + torch.matmul(kalman_gain, innovation.t()).t()
+            z_updated = z_pred_t + torch.matmul(kalman_gain, innovation.t()).t()
             z_updateds.append(z_updated)
 
             z_prev = z_updated
 
-        z_means = torch.stack(z_means, dim=1)
-        z_logvars = torch.stack(z_logvars, dim=1)
-        z_updateds = torch.stack(z_updated, dim=1)
+        mean = torch.stack(mean, dim=1)
+        logvar = torch.stack(logvar, dim=1)
 
+        z_updateds = torch.stack(z_updated, dim=1)
         x_recon = self.decoder(z_updateds.view(-1, self.latent_dim)).view(batch_size, seq_len, -1)
-        z_means = z_means.view(-1, self.latent_dim)
-        z_logvars = z_logvars.view(-1, self.latent_dim)
-        return x_recon, z_means, z_logvars
+
+        return x_recon, mean, logvar
 
 
             
